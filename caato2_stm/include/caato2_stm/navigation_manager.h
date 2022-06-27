55882758692ca0261531012335a728dfdcb8a6ff
choobacca-bacca <abilash.sivalingam95@gmail.com>
2022-06-27 13:48:16 +0800

    added navigation manager service

--- caato2_stm/include/caato2_stm/navigation_manager.h
+++ caato2_stm/include/caato2_stm/navigation_manager.h
@@ -4,21 +4,31 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <geometry_msgs/Pose.h>
+#include "caato2_stm/navigate_goal.h"
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 class NavigationManager
 {
-    public:
+public:
+  NavigationManager(ros::NodeHandle* nh_pointer);
+  ~NavigationManager();
 
-    NavigationManager();
+  void actionClientRun(const move_base_msgs::MoveBaseGoal& input_goal);
 
-    void navigate_manager_server_run();
+  void spinManager();
 
-    private:
+private:
+  ros::NodeHandle* nh;
+  MoveBaseClient ac;
+  ros::ServiceServer nav_goal_service;
 
-    MoveBaseClient ac{"move_base", true};
+  actionlib::SimpleClientGoalState goal_state{ actionlib::SimpleClientGoalState::PENDING };
 
+  bool service_state;
+
+  bool navGoal(caato2_stm::navigate_goal::Request& req, caato2_stm::navigate_goal::Response& res);
 };
 
 #endif

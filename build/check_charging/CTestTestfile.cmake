801ce7ad35001fbeee76ae90408be277de093cab
selvavignesh-2206 <b.selvavignesh@gmail.com>
2022-09-06 10:51:23 +0800

    testing

--- build/check_charging/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dimsum/caato2_ws/src/caato2/check_charging
-# Build directory: /home/dimsum/caato2_ws/src/caato2/build/check_charging
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")

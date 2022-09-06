d18735d9fa5db69fc848588ef638a7d8598bc7b0
selvavignesh-2206 <b.selvavignesh@gmail.com>
2022-09-06 13:09:30 +0800

    added delay for ds4 trigger launch

--- caato2_stm/include/caato2_stm/detection_logic.h
+++
@@ -1,9 +0,0 @@
-#include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
-#include <actionlib_msgs/GoalID.h>
-#include "geometry_msgs/PoseStamped.h"
-#include "geometry_msgs/Pose.h"
-#include "geometry_msgs/Point.h"
-
-
-// TODO: The whole damn thing
